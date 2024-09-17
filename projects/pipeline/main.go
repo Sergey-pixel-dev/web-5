ac4b4c6f479464dfa869fa9d2c2836251848d76c
Sergey-pixel-dev <sonin.serej@yandex.ru>
2024-09-17 19:25:59 +0300

    pipeline был сделан

--- projects/pipeline/main.go
+++ projects/pipeline/main.go
@@ -1,7 +1,32 @@
 package main
 
+import "fmt"
+
 // реализовать removeDuplicates(in, out chan string)
 
+func removeDuplicates(in, out chan string) {
+	defer close(out)
+	var lastString string
+	for i := range in {
+		if i != lastString {
+			out <- i
+		}
+		lastString = i
+	}
+}
+
 func main() {
-	// здесь должен быть код для проверки правильности работы функции removeDuplicates(in, out chan string)
+	var in = make(chan string)
+	var out = make(chan string)
+	go removeDuplicates(in, out)
+	go func() {
+		defer close(in)
+		for _, i := range "1112222223333333444444" {
+			in <- string(i)
+		}
+	}()
+	for i := range out {
+		fmt.Print(i)
+	}
+
 }
